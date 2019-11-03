bf7c1fbdcc617ae09904161ca6162cca1bc57719
Hojei Hsu(PC) <hhhhojeihsu@gmail.com>
2019-11-04 01:36:08 +0800

    Script Now Do Self-elevating

--- WinNICMetrics.ps1
+++ WinNICMetrics.ps1
@@ -1,3 +1,10 @@
+# Reference: https://stackoverflow.com/a/57035712
+if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+  exit;
+}
+
 function main()
 {
   # Show NIC
